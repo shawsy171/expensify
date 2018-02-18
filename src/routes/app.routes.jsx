40511c9446916beaed18c7ce30c1dc6b3986f603
Damien Shaw <damienshaw@tengroup.com>
2018-02-18 17:54:32 +0000

    refactor(modules): add modules folder
    added a modules folder the make the set up cleaner

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,11 +1,12 @@
+// @ts-check
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 // pages
-import DashBoardPage from './../Dashboard/containers/dashboard-page.jsx';
-import AddExpensePage from './../Dashboard/containers/add-expense/add-expense-page.jsx';
-import EditExpensePage from './../Dashboard/containers/edit-expense/edit-expense-page.jsx';
-import HelpPage from './../Site/containers/help-page.jsx';
+import DashBoardPage from './../modules/dashboard/containers/dashboard-page.jsx';
+import AddExpensePage from './../modules/dashboard/containers/add-expense/add-expense-page.jsx';
+import EditExpensePage from './../modules/dashboard/containers/edit-expense/edit-expense-page.jsx';
+import HelpPage from './../modules/site/containers/help-page.jsx';
 
 const routes = (
   <BrowserRouter>
