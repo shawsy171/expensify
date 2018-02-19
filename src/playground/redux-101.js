b55d1c41cf0c2d6d359f4cb0d4c1252b72b31c5e
Damien Shaw <damienshaw@tengroup.com>
2018-02-19 00:18:57 +0000

    feat(playground): redux 101
    basic redux added to the playground folder

---
+++ src/playground/redux-101.js
@@ -0,0 +1,43 @@
+import { createStore } from 'redux';
+
+const store = createStore((state = { count: 0 }, action) =>  {
+  // console.log('Store Called', action.type);
+
+  switch (action.type) {
+    case 'INCREMENT': 
+      return { count: state.count + 1 }
+
+    case 'DECREMENT':
+      return { count: state.count - 1 }
+
+    case 'RESET':
+      return { count: 0 }
+
+    default:
+      return state;
+  }
+});
+
+store.subscribe((data) => {
+  console.log(data);
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+store.dispatch({
+  type: 'INCREMENT',
+});
+
+store.dispatch({
+  type: 'RESET',
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+});
