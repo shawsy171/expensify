a6596f3f674fe5ced3e5a53ba92b664dfd4834f3
Damien Shaw <damienshaw@tengroup.com>
2018-02-25 20:50:11 +0000

    docs(playground): higher order component
    added comments to the higher order components

--- src/playground/higher-order-components.js
+++ src/playground/higher-order-components.js
@@ -16,6 +16,16 @@ const Info = (props) => (
   </div>
 );
 
+/** Higher Order Component
+ * takes in a component in this case it is <Info/>
+ * if the user is admin they will see the info
+ * else they will see a message
+ * 
+ * @example withAdminWarning(Info)
+ * 
+ * @returns a new component which can now be rendered as normal
+ */
+
 const withAdminWarning = (WrappedComponent) => {
   const isAdmin = (showMessage) => (
     showMessage && <p>Please do not share this infomation it is private</p>
@@ -30,6 +40,15 @@ const withAdminWarning = (WrappedComponent) => {
 
 const AdminInfo = withAdminWarning(Info);
 
+/** Higher Order Component
+ * takes in a component in this case it is <Info/>
+ * if the user is logged in they can see the info
+ * else they will see a message
+ * 
+ * @example requireAuthentication(Info)
+ * 
+ * @returns a new component which can now be rendered as normal
+ */
 const requireAuthentication = (WrappedComponent) => {
   const hasLoggedIn = (props) => (
     props.isAuthenticated ? <WrappedComponent {...props}/> : <p>Please log in to see the info</p>
