90afd5cb4aa5e216bdd9f8a2283a01b946629e78
Damien Shaw <damienshaw@tengroup.com>
2018-03-03 15:46:15 +0000

    refactor(expense): moved expense-list-item
    moved expense-list-item into expense-list

--- src/features/expense/components/expense-list/expense-list.jsx
+++ src/features/expense/components/expense-list/expense-list.jsx
@@ -1,36 +1,46 @@
-// @ts-check
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // store
 import { connect } from 'react-redux';
 
 // components
-import ExpenseListItem from './components/expense-list-item/expense-list-item.jsx';
+import ExpenseListItem from './expense-list-item/expense-list-item';
 
 // selectors
 import selectorExpenses from './../../store/expense/selectors';
 
 const ExpenseList = (props) => {
+  const { expenses } = props;
+
   const mapList = () => (
-    props.expenses.map((expense, index) => 
-      <ExpenseListItem key={index} expense={expense} />
-    )
-  )
-  
+    expenses.map(expense =>
+      <ExpenseListItem key={expense.id} expense={expense} />)
+  );
+
   return (
     <div>
       <h1>Expense List</h1>
       { mapList() }
     </div>
-  )
-}
+  );
+};
 
-const mapStateToProps = (state) => (
-  {
-    expenses: selectorExpenses(state.expenses, state.filters),
-  }
-)
+ExpenseList.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    description: PropTypes.string,
+    note: PropTypes.string,
+    amount: PropTypes.oneOf([
+      'date',
+      'amount',
+    ]),
+    createdAt: PropTypes.string,
+  })).isRequired,
+};
+
+const mapStateToProps = state => (
+  { expenses: selectorExpenses(state.expenses, state.filters) }
+);
+
 
 export default connect(mapStateToProps)(ExpenseList);
-
-
