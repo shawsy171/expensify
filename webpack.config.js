fc62ef656915b792186de928f7d231a341039444
Damien Shaw <damienshaw@tengroup.com>
2018-03-04 22:56:28 +0000

    chore(app): css build
    css build plus a change to css source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const outputFile = path.join(__dirname, 'public');
 
 
 module.exports = (env) => {
   const isProduction = (env === 'production');
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     // entry must be defined in webpack so webpack know with to start
@@ -31,16 +33,28 @@ module.exports = (env) => {
         exclude: /node_modules/,
       }, {
         test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
+        use: CSSExtract.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        }),
       }],
     },
-
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
-
+    plugins: [
+      CSSExtract,
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: outputFile,
       historyApiFallback: true,
